da5e936008ba3162cbf4efba8f17550bdd0693e6
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-04 16:45:58 -0400

    Update 2_🏀_NBA.py

--- pages/2_🏀_NBA.py
+++ pages/2_🏀_NBA.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.header('NBA')
+st.set_page_config(page_title="NBA Predictions", layout="wide")
+
+st.title("NBA Predictions")
+st.markdown("""
+Here are the latest predictions for NBA games.
+""")
