6a907e00f5d954d0d28aa28ff7a697e039dbf544
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-10 16:48:57 -0400

    Update 1_⚾_MLB.py

--- pages/1_⚾_MLB.py
+++ pages/1_⚾_MLB.py
@@ -130,6 +130,10 @@ def scrape_games():
     return games, None
 
 def generate_predictions():
+    def daterange(start_date, end_date):
+        for n in range(int((end_date - start_date).days)):
+            yield start_date + timedelta(n)
+
     bat_data = []
     for team in teams:
         df = fetch_and_process_batting_data(team, 2024)
