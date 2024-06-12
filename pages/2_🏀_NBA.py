7a348d8cbda5f34a01830b79c3d5671afff56ada
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-12 09:29:25 -0400

    Update 2_🏀_NBA.py

--- pages/2_🏀_NBA.py
+++ pages/2_🏀_NBA.py
@@ -17,7 +17,42 @@ st.sidebar.write("""
     This application provides predictions for today's NBA games based on historical data and machine learning models. 
     The predictions include the expected winner, starting pitchers, and the odds for each game.
 """)
-
+st.markdown("""
+    <style>
+    .coming-soon-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 80vh;
+        background: linear-gradient(135deg, #f3ec78, #af4261);
+        color: black;
+        font-family: 'Arial', sans-serif;
+        text-align: center;
+        border-radius: 15px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    }
+    .coming-soon-text {
+        font-size: 4em;
+        font-weight: bold;
+    }
+    .coming-soon-subtext {
+        font-size: 1.5em;
+        margin-top: 20px;
+    }
+    .emoji {
+        font-size: 2em;
+        margin: 0 10px;
+    }
+    </style>
+    <div class="coming-soon-container">
+        <div>
+            <div class="emoji">🚧</div>
+            <div class="coming-soon-text">Coming Soon</div>
+            <div class="emoji">🚧</div>
+            <div class="coming-soon-subtext">Stay tuned for upcoming earnings!</div>
+        </div>
+    </div>
+""", unsafe_allow_html=True)
 # Add footer with additional links or information
 st.markdown("""
     <div class="footer">
