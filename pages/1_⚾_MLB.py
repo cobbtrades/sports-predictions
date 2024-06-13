eeaf5f71ce0ef2ce50aa76b7955c4d8e16826d0e
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-13 18:01:54 -0400

    Update 1_⚾_MLB.py

--- pages/1_⚾_MLB.py
+++ pages/1_⚾_MLB.py
@@ -318,14 +318,14 @@ def generate_predictions():
     todays_games['Predicted Winner'] = todays_games.apply(lambda row: row['Tm'] if row['Predicted Outcome'] == 'Win' else row['Opp'], axis=1)
     todays_games['Confidence'] = predicted_confidence
     todays_games.dropna(inplace=True)
-    display_df = todays_games[['Tm', 'Opp', 'Tm_ml', 'Opp_ml', 'Predicted Winner', 'Confidence', 'TmStart', 'OppStart']].copy()
-    display_df.rename(columns={'Tm': 'Home Team','Opp': 'Away Team','Tm_ml': 'Home Odds','Opp_ml': 'Away Odds','Predicted Winner': 'Predicted Winner','Confidence': 'Confidence','TmStart': 'Home Pitcher','OppStart': 'Away Pitcher'}, inplace=True)
+    display_df = todays_games[['Tm', 'Opp', 'Tm_ml', 'Opp_ml', 'Predicted Winner', 'TmStart', 'OppStart']].copy()
+    display_df.rename(columns={'Tm': 'Home Team','Opp': 'Away Team','Tm_ml': 'Home Odds','Opp_ml': 'Away Odds','Predicted Winner': 'Predicted Winner','TmStart': 'Home Pitcher','OppStart': 'Away Pitcher'}, inplace=True)
     display_df['Losing Team'] = display_df.apply(lambda row: row['Away Team'] if row['Predicted Winner'] == row['Home Team'] else row['Home Team'], axis=1)
     display_df['Matchup'] = display_df.apply(lambda row: f"{row['Predicted Winner']} vs {row['Losing Team']}", axis=1)
     display_df['Winner Odds'] = display_df.apply(lambda row: row['Home Odds'] if row['Predicted Winner'] == row['Home Team'] else row['Away Odds'], axis=1)
     display_df['Winner Odds'] = display_df['Winner Odds'].astype(float).astype(int)
     display_df['Confidence'] = (display_df['Confidence'] * 100).round(2).astype(str) + '%'
-    final_display_columns = ['Matchup', 'Home Pitcher', 'Away Pitcher', 'Predicted Winner', 'Winner Odds', 'Confidence']
+    final_display_columns = ['Matchup', 'Home Pitcher', 'Away Pitcher', 'Predicted Winner', 'Winner Odds']
     final_display_df = display_df[final_display_columns]
     return final_display_df
 
@@ -370,10 +370,7 @@ if st.session_state.predictions is not None:
             ],
             'Winner Odds': [
                 {'selector': 'td', 'props': 'background-color: #000000; color: #2daefd; font-weight: bold;'},
-            ],
-            'Confidence': [
-                {'selector': 'td', 'props': 'background-color: #000000; color: #ff00ff; font-weight: bold;'},
-            ],
+            ]
         }
     ).set_properties(**{'text-align': 'center'}).hide(axis='index')
     
