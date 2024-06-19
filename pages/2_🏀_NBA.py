8a4e50e3b06ff5c35bc15a550da0a5b2e1569846
cobbtrades <135261452+cobbtrades@users.noreply.github.com>
2024-06-19 08:40:43 -0400

    Update 2_🏀_NBA.py

--- pages/2_🏀_NBA.py
+++ pages/2_🏀_NBA.py
@@ -1,21 +1,10 @@
 import streamlit as st
-import requests, pandas as pd, time, numpy as np, pickle, re, json
-from datetime import datetime, timedelta
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.impute import SimpleImputer
-import altair as alt
 
+st.set_page_config(page_title="NFL Predictions", layout="wide")
 
-# Add sidebar with additional information or navigation
-st.sidebar.header('About')
-st.sidebar.write("""
-    This application provides predictions for today's NBA games based on historical data and machine learning models. 
-    The predictions include the expected winner, starting pitchers, and the odds for each game.
+st.title("NBA Predictions")
+st.markdown("""
+Here are the latest predictions for NBA games.
 """)
 st.markdown("""
     <style>
@@ -50,11 +39,5 @@ st.markdown("""
             <div class="coming-soon-text">Coming Soon</div>
             <div class="emoji">🚧</div>
         </div>
-    </div>
-""", unsafe_allow_html=True)
-# Add footer with additional links or information
-st.markdown("""
-    <div class="footer">
-        <p>&copy; 2024 Cobb's ML Predictions. All rights reserved.</p>
     </div>
 """, unsafe_allow_html=True)
